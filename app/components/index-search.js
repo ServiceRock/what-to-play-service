58aa48ef0beaf8fc103c14d9343205f3c7570165
Ihor Ostapchuk <iostapchyk@chegg.com>
2019-07-28 18:53:10 +0300

    Fixing build error: @fortawesome/fontawesome-svg-core ENOENT: no such file or directory ->https://github.com/tonycoco/heroku-buildpack-ember-cli

--- app/components/index-search.js
+++ app/components/index-search.js
@@ -1,4 +1,28 @@
 import Component from '@ember/component';
+import Ember from 'ember';
 
 export default Component.extend({
+	store: Ember.inject.service(), // we will use Ember.store for search
+	foundItems: null, // we need to store found items
+
+	findItem(value) {
+		// we get `store` service to search
+		// we get `model` to know which model to query
+		//    and `key` to narrow down a search to one attribute
+		const { store, model, key } = this.getProperties('store', 'model', 'key');
+
+		let query = {};
+		query[key] = value;
+
+		// we query the data and set the results as ‘foundItems’
+		store.query(model, query).then((items) => {
+			this.set('foundItems', items);
+		});
+	},
+
+	actions: {
+		keyUp(value) {
+			this.findItem(value);
+		}
+	}
 });
