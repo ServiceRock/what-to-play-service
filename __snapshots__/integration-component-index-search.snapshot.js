dc1be6acf358ef07f6a1780907fe1e71f94e130d
Ihor Ostapchuk <iostapchyk@chegg.com>
2019-07-30 00:10:11 +0300

    Updated tests, make Travic CI npm run test more happy :)

---
+++ __snapshots__/integration-component-index-search.snapshot.js
@@ -0,0 +1,9 @@
+exports['it-renders--indexsearch-component-with-no-parameters'] = `
+<div class="ember-view"><div class="ember-view"><div class="site-header">
+  <div class="input-wrapper">
+    <input key-up="keyUp" placeholder="Введіть назву гри" class="search-field" type="text">
+
+    <div class="focus-background"></div>
+  </div>
+</div></div></div>
+`
---
+++ __snapshots__/integration-component-last-proposals.snapshot.js
@@ -0,0 +1,3 @@
+exports['it-renders--lastproposals-component-with-no-parameters'] = `
+<div class="ember-view"><div class="list-group-item d-flex justify-content-between align-items-center ember-view"> - </div></div>
+`
